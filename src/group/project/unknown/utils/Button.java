4c9cf7ec936d8d282421242ad7a133b6e101c64f
Wouix <hampus@wouix.se>
2015-03-24 18:29:45 +0100

    Added comments to Button class.

--- src/group/project/unknown/utils/Button.java
+++ src/group/project/unknown/utils/Button.java
@@ -25,17 +25,40 @@ import org.newdawn.slick.opengl.*;
 
 import static org.lwjgl.opengl.GL11.*;
 
+/**
+ * Button class.
+ * 
+ * @author João Lourenço and Hampus Backman
+ */
 public class Button {
 
+	/** Rectangles for the Button */
 	private Rectangle mouseRect;
 	private Rectangle rect;
 
+	/** Texture holder for the Button. */
 	private Texture tex;
 
+	/** Red, Green, Blue color for the Button. */
 	private float r;
 	private float g;
 	private float b;
 
+	/**
+	 * The constructor for the Button.
+	 * 
+	 * @param x
+	 *            , X Position.
+	 * @param y
+	 *            , Y Position.
+	 * @param w
+	 *            , Width.
+	 * @param h
+	 *            , Height.
+	 * @param texUrl
+	 *            , Url for the desired texture.
+	 * @author João Lourenço and Hampus Backman
+	 */
 	public Button(int x, int y, int w, int h, String texUrl) {
 		this.mouseRect = new Rectangle(0, 0, 1, 1);
 		this.rect = new Rectangle(x, y, w, h);
@@ -43,17 +66,34 @@ public class Button {
 		this.tex = Loader.loadTexture(texUrl);
 	}
 
+	/**
+	 * Checks if the Button is being hovered.
+	 * 
+	 * @return
+	 * @author João Lourenço and Hampus Backman
+	 */
 	public boolean hover() {
 		mouseRect.move(Mouse.getX(), Registry.getScreenHeight() - Mouse.getY());
 		if (rect.intersects(mouseRect)) return true;
 		return false;
 	}
 
+	/**
+	 * Checks if The Button is clicked.
+	 * 
+	 * @return
+	 * @author João Lourenço and Hampus Backman
+	 */
 	public boolean clicked() {
 		if (hover() && Mouse.isButtonDown(0)) return true;
 		return false;
 	}
 
+	/**
+	 * Renders the Button.
+	 * 
+	 * @author João Lourenço and Hampus Backman
+	 */
 	public void render() {
 		glColor3f(r, g, b);
 		glBindTexture(GL_TEXTURE_2D, tex.getTextureID());
@@ -66,6 +106,17 @@ public class Button {
 		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 
+	/**
+	 * Sets the color of the Button.
+	 * 
+	 * @param r
+	 *            , Red.
+	 * @param g
+	 *            , Green.
+	 * @param b
+	 *            , Blue.
+	 * @author João Lourenço and Hampus Backman
+	 */
 	public void setColor(float r, float g, float b) {
 		this.r = r;
 		this.g = g;
